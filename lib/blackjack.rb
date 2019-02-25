b11c42b548dae77268350c2561afcca537996903
Roni Shabo <roni.shabo@gmail.com>
2019-02-25 19:29:51 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -52,15 +52,15 @@ end
 # get every test to pass before coding runner below #
 #####################################################
 
-# def runner
-#  welcome 
-#  card_total = initial_round
-#  until card_total > 21
-#    card_total = hit?(card_total)
-#    display_card_total(card_total)
-#  end
-#  end_game(card_total)
-# end
+def runner
+  welcome 
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
+ end
 
 # runner
 

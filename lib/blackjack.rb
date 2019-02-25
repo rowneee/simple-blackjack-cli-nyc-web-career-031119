23210c44a0cf85f57ad4b15631390be1ef9dd445
Roni Shabo <roni.shabo@gmail.com>
2019-02-25 17:56:57 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -13,12 +13,11 @@ def display_card_total(card_total)
 end
 
 def prompt_user
-  gets.chomp
   puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
 def end_game
